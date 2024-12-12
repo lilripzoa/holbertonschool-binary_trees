336dcb319efcaf7832d7baab28d6485d49e74874
root <root@DESKTOP-46AJ964>
2024-12-12 15:13:02 +0100

    exo

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,49 +2,36 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
-/**
- * height - function calcul the height of a tree.
- * @tree: Pointer to the root node of the tree.
- * Return: The height of the tree.
- */
-int height(const binary_tree_t *tree)
-{
-	int left_height;
-	int right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = height(tree->left);
-	right_height = height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
-}
 
 /**
- * is_perfect_recursive -  check if binary is perfect
+ * is_perfect - check if binary is perfect
  * @tree: Pointer to the root node of the tree.
  * @depth: The depth of the tree
  * @level: The current level of the node in the tree.
  * Return: 1 if tree is perfect, 0 otherwise
  */
 
-int is_perfect_recursive(const binary_tree_t *tree, int depth, int level)
+int is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (1);
+	int left;
+	int right;
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (depth == level);
-
-	if (tree->left == NULL || tree->right == NULL)
+	if (tree->left && tree->right)
+	{
+		left = 1 + is_perfect(tree->left);
+		right = 1 + is_perfect(tree->right);
+		if (right == left && right != 0 && left != 0)
+			return (right);
 		return (0);
-
-	return (is_perfect_recursive(tree->left, depth, level + 1) &&
-			is_perfect_recursive(tree->right, depth, level + 1));
+	}
+	else if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
 /**
@@ -55,18 +42,19 @@ int is_perfect_recursive(const binary_tree_t *tree, int depth, int level)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int depth = 0;
-	const binary_tree_t *temp;
+	int result;
 
 	if (tree == NULL)
-		return (0);
-
-	temp = tree;
-	while (temp != NULL)
 	{
-		depth++;
-		temp = temp->left;
+		return (0);
 	}
-
-	return (is_perfect_recursive(tree, depth, 0));
+	else
+	{
+		result = is_perfect(tree);
+		if (result != 0)
+		{
+			return (1);
+		}
+		return (0);
+	}
 }
